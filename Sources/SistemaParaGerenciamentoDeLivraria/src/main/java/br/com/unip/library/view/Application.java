4636f5ea2c8963ff6b9b059d4bba306cfc9e2442
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-01 20:54:10 -0300

    feat: hello-world.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
@@ -1,11 +1,9 @@
 package br.com.unip.library.view;
 
-import br.com.unip.library.service.book.BookServiceImpl;
-
 public class Application {
 
   public static void main(String[] args) {
-
+    System.out.println("Hello World.");
   }
 
 }
