eee7a6a7757714b0a2f87108e998e5b7359891f5
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-04 20:43:33 -0300

    fix: removal of unused code.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
@@ -3,11 +3,7 @@ package br.com.unip.library.view;
 public class Application {
 
   public static void main(String[] args) {
-    sayHello();
-  }
 
-  public static void sayHello(){
-    System.out.println("Hello world!");
   }
 
 }
