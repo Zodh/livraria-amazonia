67923d25593490d8753e8a185d3605d5da21f4e2
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-04 20:47:16 -0300

    feat: author service layer without implementation.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/service/author/AuthorServiceImpl.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/service/author/AuthorServiceImpl.java
@@ -1,5 +1,32 @@
 package br.com.unip.library.service.author;
 
-public class AuthorServiceImpl {
+import br.com.unip.library.model.entity.Author;
+import java.util.List;
 
+public class AuthorServiceImpl implements AuthorService{
+
+  @Override
+  public void create(Author author) {
+
+  }
+
+  @Override
+  public List<Author> listAll() {
+    return null;
+  }
+
+  @Override
+  public Author findById(Integer id) {
+    return null;
+  }
+
+  @Override
+  public void update(String name, String fname) {
+
+  }
+
+  @Override
+  public void delete(Integer id) {
+
+  }
 }
