b9502220ce20d6de887c24f3eeb99a51f0a480db
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-04 21:20:21 -0300

    feat: author business object.

---
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/model/bo/AuthorBO.java
@@ -0,0 +1,57 @@
+package br.com.unip.library.model.bo;
+
+import br.com.unip.library.exception.ExceptionErrorEnum;
+import br.com.unip.library.exception.LibraryException;
+import br.com.unip.library.model.entity.Author;
+import lombok.Getter;
+
+@Getter
+public class AuthorBO {
+
+  private final String name;
+
+  private final String fname;
+
+  public AuthorBO(Author author) {
+    this.name = author.getName();
+    this.fname = author.getFname();
+    checkIfNameIsValid();
+    checkIfFNameIsValid();
+  }
+
+  public Author toAuthor(){
+    return Author
+        .builder()
+        .name(name)
+        .fname(fname)
+        .build();
+  }
+
+  private void checkIfNameIsValid() {
+    var message = "";
+    if (this.name == null || this.name.isBlank()) {
+      message = "Author name is null, empty or just whitespaces!";
+    }
+    if (!message.equals("")) {
+      throw LibraryException
+          .builder()
+          .action(ExceptionErrorEnum.CREATE_BOOK)
+          .message(message)
+          .build();
+    }
+  }
+
+  private void checkIfFNameIsValid() {
+    var message = "";
+    if (this.name == null || this.fname.isBlank()) {
+      message = "Author name is null, empty or just whitespaces!";
+    }
+    if (!message.equals("")) {
+      throw LibraryException
+          .builder()
+          .action(ExceptionErrorEnum.CREATE_BOOK)
+          .message(message)
+          .build();
+    }
+  }
+}
