289696c375c4cf023b4f9a50289b94e876a9cd1e
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-04 23:14:26 -0300

    feat: inclusion of more possible exceptions.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/exception/ExceptionErrorEnum.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/exception/ExceptionErrorEnum.java
@@ -2,11 +2,19 @@ package br.com.unip.library.exception;
 
 public enum ExceptionErrorEnum {
 
-  CREATE_BOOK("Create Book"),
-  LIST_BOOKS("List All Books"),
-  FIND_BOOK_BY_ID("Find a Book by ID"),
-  UPDATE_BOOK("Update Book"),
-  DELETE_BOOK("Delete Book");
+  CREATE_BOOK("Service Create Book"),
+  CREATE_BOOK_BO("Create Book Business Object"),
+  LIST_BOOKS("Service List All Books"),
+  FIND_BOOK_BY_ISBN("Service Find a Book by ISBN"),
+  UPDATE_BOOK("Service Update Book"),
+  DELETE_BOOK("Service Delete Book"),
+
+  CREATE_AUTHOR("Service Create Author"),
+  CREATE_AUTHOR_BO("Create Author Business Object"),
+  LIST_AUTHORS("Service List All Authors"),
+  FIND_AUTHOR_BY_ID("Service Find an Author by ID"),
+  UPDATE_AUTHOR("Service Update Author"),
+  DELETE_AUTHOR("Service Delete Author");
 
   public final String actionLabel;
 
