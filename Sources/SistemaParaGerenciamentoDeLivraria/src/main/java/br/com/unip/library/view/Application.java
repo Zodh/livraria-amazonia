e5c153cc8a642e57018bc907078b7409a67bbb9c
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-06 00:53:19 -0300

    test: webhook test.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/Application.java
@@ -1,8 +1,24 @@
 package br.com.unip.library.view;
 
+import br.com.unip.library.service.publisher.PublisherServiceImpl;
+import br.com.unip.library.util.LibraryUtils;
+
 public class Application {
 
   public static void main(String[] args) {
+
+    var publisherService = new PublisherServiceImpl();
+
+    var randomString = "test";
+    var maskedRandomString = LibraryUtils.maskString(randomString, 0, 4);
+
+
+
+    System.out.println(randomString);
+    System.out.println(maskedRandomString);
+
+    publisherService.delete(12);
+
 
   }
 }
