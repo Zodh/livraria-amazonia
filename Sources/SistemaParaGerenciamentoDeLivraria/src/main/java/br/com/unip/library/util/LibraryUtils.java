20cc599205bd5d3c2aa8f6d583d3e09d5acf8190
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-06 00:14:29 -0300

    feat: mask string function.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/util/LibraryUtils.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/util/LibraryUtils.java
@@ -1,11 +1,41 @@
 package br.com.unip.library.util;
 
+import br.com.unip.library.exception.ExceptionErrorEnum;
+import br.com.unip.library.exception.LibraryException;
 import javax.swing.JOptionPane;
 
 public class LibraryUtils {
 
-  public static void showInfo(String title, String message){
+  public static void showInfo(String title, String message) {
     JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
   }
 
+  public static String maskString(String text, Integer start, Integer end) {
+    try {
+      if (text == null || text.equals("")) {
+        return "";
+      }
+      if (start < 0) {
+        start = 0;
+      }
+      if (end > text.length()) {
+        end = text.length();
+      }
+      if (start > end) {
+        throw new Exception("End index cannot be greater than start index.");
+      }
+      var maskLength = end - start;
+      if (maskLength == 0) {
+        return text;
+      }
+      var sbMaskString = new StringBuilder(maskLength);
+      sbMaskString.append("*".repeat(maskLength));
+      return text.substring(0, start)
+          + sbMaskString
+          + text.substring(start + maskLength);
+    } catch (Exception exception) {
+      throw new LibraryException("Error Trying to Mask an Information",
+          ExceptionErrorEnum.MASK_INFO);
+    }
+  }
 }
