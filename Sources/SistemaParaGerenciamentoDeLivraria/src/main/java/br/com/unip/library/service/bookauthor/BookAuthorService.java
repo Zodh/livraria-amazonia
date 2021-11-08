8c17c56882c3ad66f92abb35337e398a159a449a
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-07 23:05:51 -0300

    feat: book author interface methods.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/service/bookauthor/BookAuthorService.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/service/bookauthor/BookAuthorService.java
@@ -2,4 +2,12 @@ package br.com.unip.library.service.bookauthor;
 
 public interface BookAuthorService {
 
+  Boolean createBookAuthorByIsbn(String isbn, Integer authorId);
+
+  void deleteBookAuthorByIsbn(String isbn);
+
+  void deleteBookAuthorsByAuthorId(Integer id);
+
+  Integer generateSeqNo();
+
 }
