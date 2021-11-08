ea9de53ac13ecc5e4eb87253cd9cedf05fea2103
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-07 23:02:41 -0300

    feat: book author find by isbn and author id

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/BookAuthorDAO.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/BookAuthorDAO.java
@@ -2,6 +2,9 @@ package br.com.unip.library.dao;
 
 import br.com.unip.library.dao.base.BaseDAO;
 import br.com.unip.library.dao.base.GenericDAO;
+import br.com.unip.library.dao.base.HibernateUtil;
+import br.com.unip.library.model.entity.Author;
+import br.com.unip.library.model.entity.Book;
 import br.com.unip.library.model.entity.BookAuthor;
 
 public class BookAuthorDAO extends BaseDAO<BookAuthor, Integer> implements
@@ -9,5 +12,21 @@ public class BookAuthorDAO extends BaseDAO<BookAuthor, Integer> implements
 
   public BookAuthorDAO() {
     super(BookAuthor.class);
+  }
+
+  public Author findByAuthorId(Integer id) {
+    beginTransaction();
+    var session = HibernateUtil.getSession();
+    var author = session.get(Author.class, id);
+    endTransaction();
+    return author;
+  }
+
+  public Book findByBookIsbn(String isbn) {
+    beginTransaction();
+    var session = HibernateUtil.getSession();
+    var book = session.get(Book.class, isbn);
+    endTransaction();
+    return book;
   }
 }
