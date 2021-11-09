88dfa859b1114ec9f36dfabdd08d2ad8f4359920
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-08 22:39:45 -0300

    fix: delete by isbn or author id fixed.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/BookAuthorDAO.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/BookAuthorDAO.java
@@ -3,30 +3,35 @@ package br.com.unip.library.dao;
 import br.com.unip.library.dao.base.BaseDAO;
 import br.com.unip.library.dao.base.GenericDAO;
 import br.com.unip.library.dao.base.HibernateUtil;
-import br.com.unip.library.model.entity.Author;
-import br.com.unip.library.model.entity.Book;
 import br.com.unip.library.model.entity.BookAuthor;
+import java.net.Proxy.Type;
 
-public class BookAuthorDAO extends BaseDAO<BookAuthor, Integer> implements
-    GenericDAO<BookAuthor, Integer> {
+public class BookAuthorDAO extends BaseDAO<BookAuthor, Type> implements
+    GenericDAO<BookAuthor, Type> {
 
   public BookAuthorDAO() {
     super(BookAuthor.class);
   }
 
-  public Author findByAuthorId(Integer id) {
-    beginTransaction();
-    var session = HibernateUtil.getSession();
-    var author = session.get(Author.class, id);
+  public void deleteByIsbn(String isbn) {
+    var criteriaBuilder = HibernateUtil.getSession().getCriteriaBuilder();
+    var criteriaQuery = criteriaBuilder.createQuery(BookAuthor.class);
+    var root = criteriaQuery.from(BookAuthor.class);
+    criteriaQuery.select(root).where(root.get("isbn").in(isbn));
+    var query = HibernateUtil.getSession().createQuery(criteriaQuery);
+    var result = query.getResultList();
+    result.forEach(this::delete);
     endTransaction();
-    return author;
   }
 
-  public Book findByBookIsbn(String isbn) {
-    beginTransaction();
-    var session = HibernateUtil.getSession();
-    var book = session.get(Book.class, isbn);
+  public void deleteByAuthorId(Integer id) {
+    var criteriaBuilder = HibernateUtil.getSession().getCriteriaBuilder();
+    var criteriaQuery = criteriaBuilder.createQuery(BookAuthor.class);
+    var root = criteriaQuery.from(BookAuthor.class);
+    criteriaQuery.select(root).where(root.get("author_id").in(id));
+    var query = HibernateUtil.getSession().createQuery(criteriaQuery);
+    var result = query.getResultList();
+    result.forEach(this::delete);
     endTransaction();
-    return book;
   }
 }
