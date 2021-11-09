49e4e79ec4ccb1cf622d228f7d8c40c69e7fb9ae
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-08 22:40:18 -0300

    feat: id fields and serializable implementation.

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/model/entity/BookAuthor.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/model/entity/BookAuthor.java
@@ -1,10 +1,10 @@
 package br.com.unip.library.model.entity;
 
 import com.google.gson.Gson;
+import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -19,18 +19,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "booksauthors")
 @Builder(access = AccessLevel.PUBLIC)
-public class BookAuthor {
+public class BookAuthor implements Serializable {
 
+  @Id
   @Column(name = "isbn")
   private String isbn;
 
+  @Id
   @Column(name = "author_id")
   private Integer authorId;
 
-  @Id
   @Column(name = "seq_no")
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer seqNo;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BookAuthor that = (BookAuthor) o;
+    return Objects.equals(isbn, that.isbn) && Objects
+        .equals(authorId, that.authorId) && Objects.equals(seqNo, that.seqNo);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(isbn, authorId, seqNo);
+  }
 
   @Override
   public String toString() {
