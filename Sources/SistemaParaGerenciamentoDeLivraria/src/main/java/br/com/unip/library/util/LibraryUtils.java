7712c694a8d6ff45ced2d2c3c626f0d51df19e9f
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-09 21:37:05 -0300

    refactor: removal of regex validation for authors

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/util/LibraryUtils.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/util/LibraryUtils.java
@@ -40,15 +40,6 @@ public class LibraryUtils {
     }
   }
 
-  public static Boolean isValidAuthorsInput(String authors) {
-    var pattern = Pattern.compile("^[0-9]?(?:,\\d+?)*$");
-    if (Boolean.FALSE.equals(pattern.matcher(authors).matches())) {
-      throw new LibraryException("Input only numbers and comma",
-          ExceptionErrorEnum.INVALID_INPUT_NBCM);
-    }
-    return true;
-  }
-
   public static Boolean isOnlyNumbers(String numbers) {
     var pattern = Pattern.compile("^[0-9]*$");
     if (Boolean.FALSE.equals(pattern.matcher(numbers).matches())) {
