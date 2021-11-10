f3ff8b21f68980f64e5a1410987e88a6576ed032
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-09 21:36:36 -0300

    feat: integrator class

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/integration/Integrator.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/view/integration/Integrator.java
@@ -2,36 +2,49 @@ package br.com.unip.library.view.integration;
 
 import br.com.unip.library.exception.ExceptionErrorEnum;
 import br.com.unip.library.exception.LibraryException;
-import br.com.unip.library.util.LibraryUtils;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JTextField;
 
 public class Integrator {
 
-  public static Double fromStringToDouble(String price){
-    try{
-      return Double.valueOf(price);
-    } catch (Exception exception){
-      throw new LibraryException("Invalid input on PRICE field!", ExceptionErrorEnum.INVALID_TEXT);
+  public static Integer fromStringToInteger(String publisher) {
+    try {
+      return Integer.valueOf(publisher);
+    } catch (Exception exception) {
+      throw new LibraryException("Invalid input on Integer field!",
+          ExceptionErrorEnum.INVALID_TEXT);
     }
   }
 
-  public static List<Integer> fromAuthorsStringToList(String string){
+  public static Double fromStringToDouble(String price) {
+    try {
+      return Double.valueOf(price);
+    } catch (Exception exception) {
+      throw new LibraryException("Invalid input on Double field!", ExceptionErrorEnum.INVALID_TEXT);
+    }
+  }
+
+  public static List<Integer> fromAuthorsStringToList(String string) {
     var list = new ArrayList<Integer>();
-    if (Boolean.TRUE.equals(LibraryUtils.isValidAuthorsInput(string))){
-      var authorsVector = string.split(",");
-      for(String value : authorsVector){
+    var authorsVector = string.split(",");
+    try {
+      for (String value : authorsVector) {
         list.add(Integer.parseInt(value));
       }
+    } catch (Exception exception) {
+      throw new LibraryException(
+          "Error trying to read authors. Invalid Input. " + exception.getMessage(),
+          ExceptionErrorEnum.INVALID_INPUT_NBCM);
     }
     return list;
   }
 
-  public static String getJTextString(JTextField textField){
+  public static String getJTextString(JTextField textField) {
     var text = textField.getText();
-    if (text == null || text.isBlank() || text.isEmpty()){
-      throw new LibraryException("Invalid input on " + textField.getName() + " field!", ExceptionErrorEnum.INVALID_TEXT);
+    if (text == null || text.isBlank() || text.isEmpty()) {
+      throw new LibraryException("Invalid input on " + textField.getName() + " field!",
+          ExceptionErrorEnum.INVALID_TEXT);
     }
     return textField.getText();
   }
