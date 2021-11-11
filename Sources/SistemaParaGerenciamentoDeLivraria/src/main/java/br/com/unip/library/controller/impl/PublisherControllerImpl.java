f1b1475faa1984edc852936686b953eb6fe1254c
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-11 00:52:24 -0300

    feat: publisher controller implementation

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/controller/impl/PublisherControllerImpl.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/controller/impl/PublisherControllerImpl.java
@@ -2,27 +2,30 @@ package br.com.unip.library.controller.impl;
 
 import br.com.unip.library.controller.PublisherController;
 import br.com.unip.library.model.entity.Publisher;
+import br.com.unip.library.service.publisher.PublisherServiceImpl;
 import java.util.List;
 
 public class PublisherControllerImpl implements PublisherController {
 
-  @Override
-  public void create(String name, String url) {
+  private PublisherServiceImpl publisherService = new PublisherServiceImpl();
 
+  @Override
+  public void create(Publisher publisher) {
+    publisherService.create(publisher);
   }
 
   @Override
   public List<Publisher> listAll() {
-    return null;
+    return publisherService.listAll();
   }
 
   @Override
   public void update(Integer id, String name, String url) {
-
+    publisherService.update(id, name, url);
   }
 
   @Override
   public void delete(Integer id) {
-
+    publisherService.delete(id);
   }
 }
