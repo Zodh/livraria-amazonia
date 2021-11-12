483f52289892e46c23c16121efb5d8064c733195
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-11 23:43:44 -0300

    feat: general log (dao, integrator and exception)

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/base/BaseDAO.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/base/BaseDAO.java
@@ -1,10 +1,15 @@
 package br.com.unip.library.dao.base;
 
+import br.com.unip.library.view.TestMethods;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.criteria.CriteriaQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BaseDAO<T, Type extends Serializable> implements GenericDAO<T, Type> {
+
+  private static final Logger log = LoggerFactory.getLogger(BaseDAO.class);
 
   private final Class<T> persistentClass;
 
@@ -15,35 +20,47 @@ public class BaseDAO<T, Type extends Serializable> implements GenericDAO<T, Type
 
   @Override
   public void delete(T entity) {
+    log.info("Starting database connection");
     beginTransaction();
+    log.info("Deleting entity in the database");
     HibernateUtil.getSession().delete(entity);
     commitTransaction();
+    log.info("Closing connection to database");
     endTransaction();
   }
 
   @Override
   public void create(T entity) {
+    log.info("Starting database connection");
     beginTransaction();
+    log.info(String.format("Registering entity (%s) in the database", entity.getClass()));
     saveTransaction(entity);
     commitTransaction();
+    log.info("Closing connection to database");
     endTransaction();
   }
 
   @Override
   public List<T> listAll() {
+    log.info("Starting database connection");
     beginTransaction();
     CriteriaQuery<T> criteriaQuery = HibernateUtil.getSession().getCriteriaBuilder()
         .createQuery(persistentClass);
     criteriaQuery.from(persistentClass);
+    log.info("Listing all entities in the database");
     var list = HibernateUtil.getSession().createQuery(criteriaQuery).getResultList();
+    log.info("Closing connection to database");
     endTransaction();
     return list;
   }
 
   @Override
   public void update(T entity) {
+    log.info("Starting database connection");
     beginTransaction();
+    log.info("Updating entity in the database");
     HibernateUtil.getSession().update(entity);
+    log.info("Closing connection to database");
     endTransaction();
   }
 
--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/exception/LibraryException.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/exception/LibraryException.java
@@ -1,8 +1,12 @@
 package br.com.unip.library.exception;
 
 import javax.swing.JOptionPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LibraryException extends RuntimeException {
+
+  private static final Logger log = LoggerFactory.getLogger(LibraryException.class);
 
   private final String message;
   private final ExceptionErrorEnum action;
@@ -11,10 +15,15 @@ public class LibraryException extends RuntimeException {
     super(message);
     this.message = message;
     this.action = action;
-    JOptionPane.showMessageDialog(
-        null,
-        String.format("Message: %s%nAction: %s", message, action),
-        "Error",
-        JOptionPane.ERROR_MESSAGE);
+    log.error(String.format("Message: %s%nAction: %s", message, action));
+    if (action != ExceptionErrorEnum.CREATE_AUTHOR_BO
+        && action != ExceptionErrorEnum.CREATE_PUBLISHER_BO
+        && action != ExceptionErrorEnum.CREATE_BOOK_BO) {
+      JOptionPane.showMessageDialog(
+          null,
+          String.format("Message: %s%nAction: %s", message, action),
+          "Error",
+          JOptionPane.ERROR_MESSAGE);
+    }
   }
 }
