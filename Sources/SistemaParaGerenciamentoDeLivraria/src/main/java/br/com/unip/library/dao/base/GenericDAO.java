ab02ae770ae6b389d2c7c61b9cb0d9afdc7463a9
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-13 03:03:35 -0300

    feat: exceptions treatment on transactions

--- Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/base/GenericDAO.java
+++ Sources/SistemaParaGerenciamentoDeLivraria/src/main/java/br/com/unip/library/dao/base/GenericDAO.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public interface GenericDAO<T, Type extends Serializable> {
 
-  void create(T entity);
+  void create(T entity) throws Exception;
 
-  void update(T entity);
+  void update(T entity) throws Exception;
 
-  void delete(T entity);
+  void delete(T entity) throws Exception;
 
-  List<T> listAll();
+  List<T> listAll() throws Exception;
 }
